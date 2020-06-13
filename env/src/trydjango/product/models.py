56b8762845b7bef254e95dcb90c0fd5cedbca683
edison0019 <edison_0019@hotmail.com>
2020-06-12 22:21:38 -0400

    working with the database in django

--- env/src/trydjango/product/models.py
+++ env/src/trydjango/product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # Create your models here.
-class product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
+class products(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(decimal_places=2,max_digits=10000)
+    summary = models.TextField(default='this is the default message')
